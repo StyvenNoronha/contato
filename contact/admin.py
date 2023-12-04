52729dc31eccccfab2de0236a7cd51e273a6867f
styven <styvenn16@gmail.com>
2023-12-04 12:56:54 -0300

    criação de usuario faker

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Category
 
 
 @admin.register(Contact)
@@ -8,3 +8,10 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('first_name', 'last_name', 'phone', 'email', 'created_date')
     search_fields = ('first_name', 'last_name', 'phone', 'email', 'created_date')
     ordering = ('first_name', 'last_name', 'phone', 'email', 'created_date')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
